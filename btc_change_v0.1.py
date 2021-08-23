1c35ea8374aaf2be79a92d013f00147926a187d1
mmr79ls <88501423+mmr79ls@users.noreply.github.com>
2021-08-23 09:15:39 +0200

    Update btc_change_v0.1.py

--- btc_change_v0.1.py
+++ btc_change_v0.1.py
@@ -172,6 +172,7 @@ if program=='BTC_change':
                 
             ex=ccxt.binance()
             symbols=closes.sort_values('count',ascending=False)['symbol'].to_list()
+            print(symbols)
             symbol=st.sidebar.radio('Symbol',symbols)
              #tf=st.selectbox('Time Frame',['1m','5m','15m','1h','4h','1d','1w','1M'])
              #percent_price=st.number_input('Enter the % from price to calculate',1.0)
