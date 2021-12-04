e72ef01a70c359ee8262b843567e331b04d2322b
mmr79ls <88501423+mmr79ls@users.noreply.github.com>
2021-12-04 12:44:35 +0200

    Update btc_change_0.2.py

--- btc_change_0.2.py
+++ btc_change_0.2.py
@@ -17,7 +17,7 @@ from crypto_func import BTC_drop_change,group_tweets,plot_bokeh,Volume_change
 from datetime import datetime
 from finta import TA
 import matplotlib.pyplot as plt
-from streamlit import legacy_caching.caching
+from streamlit.legacy_caching import caching
 import streamlit_analytics
 
 import plotly.express as px
